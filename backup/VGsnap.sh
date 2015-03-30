2f39319046d2d81537882ec3e804a45d1da577ef
Thomas H Jones II <github@xanthia.com>
2015-03-30 15:10:57 -0400

    Initial check-in * Script to snap all EBS components of a VG

---
+++ backup/VGsnap.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#
+# This script is designed to perform consistent backups of
+# - Selected EBS volumes (referenced by volume-id)
+# - Mounted filesystems  (referenced by fully-qualified directory-path)
+# - LVM volumes (referenced by VGNAME/LVNAME path-specification)
+#
+# Update the "DEFRETAIN" variable to change how long to keep backups
+#
+#
+# Dependencies:
+# - Generic: See the top-level README_dependencies.md for script dependencies
+#
+# License:
+# - This script released under the Apache 2.0 OSS License
+#
+######################################################################
+WHEREAMI=`readlink -f ${0}`
+SCRIPTDIR=`dirname ${WHEREAMI}`
+TARGVG=${1:-UNDEF}
+
+# Put the bulk of our variables into an external file so they
+# can be easily re-used across scripts
+source ${SCRIPTDIR}/commonVars.env
+
+# Check to see if a Volume group was passed...
+if [ ${TARGVG} == "UNDEF" ]
+then
+   echo "No volume-group specified. Aborting"
+   exit 1
+
+else
+
+   VALIDVG=$(vgs ${TARGVG} > /dev/null 2>&1)$?
+   # Check to see if passed Volume Group name is valid...
+   if [ ${VALIDVG} -ne 0 ]
+   then
+      echo "Specified VG not found on system. Aborting"
+      exit 1
+   fi
+fi
+
+PELIST=$(pvs --noheadings -S vg_name=${TARGVG} -o pv_name 2>&1)
+echo ${PELIST}
