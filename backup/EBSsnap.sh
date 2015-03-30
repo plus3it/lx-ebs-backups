8bee8c1936a87d22a0e4a34e7b0b6007f02ba9d5
Thomas H Jones II <github@xanthia.com>
2015-03-30 11:56:17 -0400

    Update EBSsnap.sh * Begin adding snapshotting functionality: start with EVERYTHING method

--- backup/EBSsnap.sh
+++ backup/EBSsnap.sh
@@ -33,7 +33,9 @@ EXPDATE=`date -d @${EXPBEYOND} "+%Y/%m/%d @ %H:%M"`	# Expiry horizon
 # - if not, try to create and set a usable location
 # - if can't, change LOGFILE to dump to world-writable location
 #   (e.g., /var/tmp)
+# NOT YET FUNCTIONAL
 function SetLogDest() {
+   echo "" > /dev/null
 }
 
 # Output log-data to multiple locations
@@ -45,10 +47,37 @@ function MultiLog() {
    #    permissions to log location.
    #    Uncomment when SetLogDest() is functional.
    # echo "${1}" >> ${LOGFILE}
+}
 
+# Enumerate *all* the disks attached to this instance
+function AllMyDisks() {
+   local COUNT=0
+   for ELEMENT in `aws ec2 describe-volumes --filters \
+      Name=attachment.instance-id,Values=${THISINSTID} --query \
+      Volumes[].VolumeId --output text`
+   do
+      EBSARRAY[${COUNT}]=${ELEMENT}
+      COUNT=$((${COUNT} + 1))
+   done
+}
+
+# Use this method to blindly snap *everything*
+function GottaCatchemAll() {
+   local ARRCT=${#EBSARRAY[@]}
+   local COUNT=0
+   SNAPLABEL="$(hostname -s)_${THISINSTID}_bkup-${DATESTMP}"
+   while [ ${COUNT} -lt ${ARRCT} ]
+   do
+      echo "Backup-Tag for ${EBSARRAY[${COUNT}]}: ${SNAPLABEL}"
+      COUNT=$((${COUNT} + 1))
+   done
 }
 
 MultiLog "Host Instance-ID: ${THISINSTID}"
 MultiLog "Current time in Seconds: ${CURCTIME}"
 MultiLog "Retention Horizon: ${KEEPHORIZ}"
 MultiLog "Kill if older: ${EXPBEYOND} (${EXPDATE})"
+
+AllMyDisks
+MultiLog "Found: ${#EBSARRAY[@]} attached volumes"
+GottaCatchemAll
