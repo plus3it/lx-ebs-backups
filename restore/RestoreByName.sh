58334198e86ccdca857b7750a5b2898b415a2744
Thomas H Jones II <github@xanthia.com>
2015-04-01 14:03:33 +0000

    Updated RestoreByName.sh * Added creation of EBS from snap functionality

--- restore/RestoreByName.sh
+++ restore/RestoreByName.sh
@@ -37,7 +37,7 @@ source ${SCRIPTDIR}/commonVars.env
 
 # Script-specific variables
 SNAPNAME="${1:-UNDEF}"
-INSTANCEAZ=`curl -s http://169.254.169.254/latest/placement/availability-zone/`
+INSTANCEAZ=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/`
 
 # Output log-data to multiple locations
 function MultiLog() {
@@ -53,12 +53,44 @@ then
 fi
 
 # Get list of snspshots matching "Name"
-SNAPLIST=`aws ec2 describe-snapshots --output=text --filter  \
-   "Name=description,Values=*_${THISINSTID}-bkup*" --filters  \
-   "Name=tag:Created By,Values=Automated Backup" --filters  \
-   "Name=tag:Name,Values=${SNAPNAME}" --query  \
-   "Snapshots[].SnapshotId"`
+function GetSnapList() {
+   local SNAPLIST=`aws ec2 describe-snapshots --output=text --filter  \
+      "Name=description,Values=*_${THISINSTID}-bkup*" --filters  \
+      "Name=tag:Created By,Values=Automated Backup" --filters  \
+      "Name=tag:Name,Values=${SNAPNAME}" --query  \
+      "Snapshots[].SnapshotId"`
+   
+   # Make sure our query resulted in a valid match
+   if [ "${SNAPLIST}" = "" ]
+   then
+      MultiLog "No snapshots found matching pattern \"${SNAPNAME}\". Aborting..."
+      exit 1
+   else
+      echo ${SNAPLIST}
+   fi
+}
 
-# Query template
-# aws ec2 create-volume --snapshot-id snap-c1260c83 --volume-type \
-#    standard --availability-zone us-west-2b
+# Create EBSes from snaps
+function SnapToEBS() {
+   for SNAPID in ${RESTORELST}
+   do
+      MultiLog "Creating EBS from snapshot \"${SNAPID}\"... "
+      NEWEBS=$(aws ec2 create-volume --output=text --snapshot-id ${SNAPID} \
+               --volume-type standard --availability-zone ${INSTANCEAZ} \
+               --query VolumeId)
+
+      # If EBS-creation fails, call the error
+      if [ "${NEWEBS}" = "" ]
+      then
+         MultiLog "EBS-creation failed!"
+      # Add a meaningful name to the EBS if creation succeeds
+      else
+	 MultiLog "Created EBS ${NEWEBS}"
+	 aws ec2 create-tags --resource ${NEWEBS} --tags \
+	    "Key=Name,Value=Restore of ${SNAPNAME}"
+      fi
+   done
+}
+
+RESTORELST="$(GetSnapList)"
+SnapToEBS
