cc62c6e3a484116d8cd1c04499248db91444cc34
Thomas H Jones II <github@xanthia.com>
2015-09-30 22:48:06 +0000

    Fix warning evaluation

--- GetMyDisks.sh
+++ GetMyDisks.sh
@@ -17,11 +17,11 @@ NC='\033[0m'
 
 if [[ $(test -r /proc/cmdline)$? ]]
 then
-   if [[ $(grep "xen_blkfront.sda_is_xvda=1" /proc/cmdline)$? ]]
+   if [[ $(grep "xen_blkfront.sda_is_xvda=1" /proc/cmdline)$? -eq 0 ]]
    then
       echo "Root-dev should be /dev/xvda"
    else
-      echo "Reported block devices may not be accurate"
+      printf "${RED}WARNING: Reported block devices may not be accurate${NC}\n"
    fi
 fi
    
@@ -36,16 +36,16 @@ MYDISKS=($(
         ))
 unset IFS
 
-printf "%s\t%s\t%s\t%s\n" "Size (GiB)" Volume-ID Volume-Type Block-Device
+printf "%s\t%s\t%s\t%s\n" "Size (GiB)" "EBS Volume-ID" Volume-Type Block-Device
 
 LOOP=0
 while [[ ${LOOP} -lt ${#MYDISKS[@]} ]]
 do
    DISKINFO=(${MYDISKS[${LOOP}]})
-   BLOCKDEV=$(aws --region ${AWSREGION} ec2 describe-volumes \
-              --volume-id ${DISKINFO[1]} \
-              --query "Volumes[].Attachments[].Device[]" \
-              --out text)
+   BLOCKDEV=$(readlink -f $(aws --region ${AWSREGION} ec2 \
+                describe-volumes --volume-id ${DISKINFO[1]} \
+                --query "Volumes[].Attachments[].Device[]" \
+                --out text))
    printf "%10s\t${RED}%s${NC}\t%s\t%s\n" ${DISKINFO[@]} ${BLOCKDEV}
    ((LOOP+=1))
 done
