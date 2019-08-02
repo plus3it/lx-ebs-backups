5d06d02e21852498eaa8b9e8e0987aab30ec20da
Thomas H Jones II <github@xanthia.com>
2019-08-02 08:23:50 -0400

    Update logic used for credential-fetching

--- setcred.sh
+++ setcred.sh
@@ -1,31 +1,64 @@
 #!/bin/sh
 #
-# Script to pull temporary credentials from instance metadata
 #
-############################################################
-PATH=/bin:/usr/bin:/opt/AWScli/bin
+# Script to fetch authentication tokens using an EC2's IAM instance-role
+########################################################################
+INSTANCEROLENAME="$( curl -skL http://169.254.169.254/latest/meta-data/iam/security-credentials/ )"
 
-IAMROLE=Instance-BAR
+# Miscellaneous output-engine
+function logIt {
+   local LOGSTR
+   local ERREXT
 
-# Get session auth tokens
-for AWSENV in `curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/${IAMROLE} | sed '{
-   s/^.*AccessKeyId" : "/AWS_ACCESS_KEY_ID=/
-   s/^.*"SecretAccessKey" : "/AWS_SECRET_ACCESS_KEY=/
-   s/^.*"Token" : "/AWS_SESSION_TOKEN=/
-   s/",$//
-   /" : "/d
-   /{/d
-   /\}/d
-}'`;
-do
-   export ${AWSENV}
-done
+   LOGSTR="${1}"
+   ERREXT="${2:-}"
 
-AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
-AWS_DEFAULT_REGION=$(echo ${AZ} | sed 's/[a-z]*$//')
+   # Spit out message to calling-shell if debug-mode enabled
+   if [[ ${DEBUGVAL} == true ]]
+   then
+      echo "${LOGSTR}" >&2
+   fi
 
-export AWS_DEFAULT_REGION
+   # Send to syslog if passed message-code is non-zero
+   if [[ ! -z ${ERREXT} ]] && [[ ${ERREXT} -gt 0 ]]
+   then
+      logger -st "${PROGNAME}" -p ${LOGFACIL} "${LOGSTR}"
+      exit "${ERREXT}"
+   fi
+}
 
-aws ec2 describe-instances
+function ExtractCredElement {
 
-env | grep AWS
+   local CRED_ELEM
+   CRED_ELEM="${1}"
+
+   if [[ ! -z ${CRED_ELEM} ]]
+   then
+      echo ${CRED_RETURN} | \
+        python -c 'import json,sys; \
+          obj=json.load(sys.stdin);print obj["'${CRED_ELEM}'"]'
+   else
+   fi
+}
+
+if [[ -z ${INSTANCEROLENAME}  ]]
+then
+   echo "Could not detect an attached IAM instance-role"
+   exit 1
+else
+   echo "Found an attached IAM instance-role [${INSTANCEROLENAME}]"
+   CRED_RETURN=$(
+      curl -skL http://169.254.169.254/latest/meta-data/iam/security-credentials/${INSTANCEROLENAME}/
+   )
+fi
+
+if [[ ${CRED_RETURN} =~ Success ]]
+then
+   echo "Snarfed some creds"
+   export AWS_ACCESS_KEY_ID=$( ExtractCredElement "AccessKeyId" )
+   export AWS_SECRET_ACCESS_KEY=$( ExtractCredElement "SecretAccessKey" )
+   export AWS_SESSION_TOKEN=$( echo ${CRED_RETURN} | ExtractCredElement "Token" )
+else
+   echo "Failed to snarf some creds"
+   exit 1
+fi
