2f86f684813fc0c7e323697597c1d5c28f2117ca
Thomas H Jones II <github@xanthia.com>
2019-08-22 20:40:25 +0000

    Fix for py3 pedantism

--- Extras/BulkSnap.py
+++ Extras/BulkSnap.py
@@ -55,7 +55,7 @@ else:
            )
     except getopt.GetoptError as err:
         # print help information and exit:
-        print str(err)  # will print something like "option -a not recognized"
+        print(str(err))  # will print something like "option -a not recognized"
         usage()
         sys.exit(2)
 
