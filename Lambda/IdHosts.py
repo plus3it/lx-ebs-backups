e23c561fe5ea181b40a6b210fbca23304858e4f4
Thomas H Jones II <github@xanthia.com>
2019-08-22 11:37:02 -0400

    Add basic/raw snapshotting-capability

--- Lambda/IdHosts.py
+++ Lambda/IdHosts.py
@@ -3,7 +3,7 @@ import boto3
 ec2client = boto3.client('ec2')
 
 # Finda all instances with 'BackMeUp' tag
-response = ec2client.describe_instances(
+filtered_response = ec2client.describe_instances(
     Filters=[
             {
                 'Name': 'tag-key',
@@ -14,10 +14,36 @@ response = ec2client.describe_instances(
 
 # Intialize list-var
 instance_list = []
+blockdev_list = []
+volids_list = []
 
 # Populate instance list-var
-for reservation in response['Reservations']:
+for reservation in filtered_response['Reservations']:
     for instance in reservation['Instances']:
         instance_list.append(instance['InstanceId'])
+        blockdev_list.append(instance['BlockDeviceMappings'])
 
-print instance_list
+# Walk blockdev_list to get volume-IDs
+for top in blockdev_list:
+    for next in top:
+        ebs_struct = next['Ebs']
+        volids_list.append(ebs_struct['VolumeId'])
+
+# Iterate volume-list to create snapshots
+for volume in volids_list:
+    snap_return = ec2client.create_snapshot(
+        Description='Bulk-snapshot test',
+        VolumeId=volume,
+        TagSpecifications=[
+            {
+                'ResourceType': 'snapshot',
+                'Tags': [
+                    {
+                        'Key': 'BackupName',
+                        'Value': 'Test'
+                    },
+                ]
+            }
+        ]
+    )
+    print 'Snapshot ' + snap_return['SnapshotId'] + ' started...'
