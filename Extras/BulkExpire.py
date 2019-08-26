f643c733ef420526c5e5bc6d03a0f268bc9c97b1
Thomas H Jones II <github@xanthia.com>
2019-08-26 14:21:39 -0400

    Change option-parsing method and add nuke-logic

--- Extras/BulkExpire.py
+++ Extras/BulkExpire.py
@@ -1,78 +1,113 @@
 #!/bin/env python
 import boto3
+import datetime
 import getopt
 import sys
+from optparse import OptionParser
 
 
 # Make our connections to the service
 ec2client = boto3.client('ec2')
 ec2resource = boto3.resource('ec2')
 
-expiry_days = 30
-snap_size = 0
-tag_name = ''
-tag_value = ''
 
-response = ec2client.describe_snapshots()
+# Define option-parsing information
+cmdopts = OptionParser()
+cmdopts.add_option(
+        "-d", "--days-old",
+            action="store",
+            type="int",
+            dest="exp_days",
+            default=30,
+            help="Minumum snapshot-age to expire (default: %default days)"
+    )
+cmdopts.add_option(
+        "-s", "--snapshot-size",
+            action="store",
+            type="int",
+            dest="snap_size",
+            help="Exact snapshot-size to match (in GiB)"
+    )
+cmdopts.add_option(
+        "-t", "--tag-name",
+            action="store",
+            type="string",
+            dest="tag_key",
+            default='',
+            help="Snapshot-tag key-name"
+    )
+cmdopts.add_option(
+        "-v", "--tag-value",
+            action="store",
+            type="string",
+            dest="tag_val",
+            default='',
+            help="Snapsot-tag value (substring-match; may be omitted)"
+    )
+
+# Parse the command options
+(options, args) = cmdopts.parse_args()
+exp_days = options.exp_days
+snap_size = options.snap_size
+tag_key = options.tag_key
+tag_val = options.tag_val
 
 
-# How to invoke the script
-def usage():
-    print('Usage: ' + sys.argv[0] + ' [GNU long option] [option] ...')
-    print('  Options:')
-    print('\t-d <EXPIRE_OLDER_THAN_DAYS>\t\t(MANDATORY)')
-    print('\t-h print this message')
-    print('\t-s <BACKUP_SIZE>\t\t\t(OPTIONAL)')
-    print('\t-t <BACKUP_TAG_NAME>\t\t\t(MANDATORY)')
-    print('\t-v <BACKUP_TAG_VALUE>\t\t\t(OPTIONAL)')
-    print('  GNU long options:')
-    print('\t--days-old <EXPIRE_OLDER_THAN_DAYS>\t(MANDATORY)')
-    print('\t--help print this message')
-    print('\t--snapshot-size <BACKUP_SIZE>\t\t(OPTIONAL)')
-    print('\t--tag-name <BACKUP_TAG_NAME>\t\t(MANDATORY)')
-    print('\t--tag-value <BACKUP_TAG_VALUE>\t\t(OPTIONAL)')
+## Supplementation validation of command options
+# Enforce passing of a tag_key value 
+if tag_key == '':
+    cmdopts.error("A tag-name must be specified (via -t/--tag-name) for search")
 
-# Check our argument-list
-criteria_str = '" as the instance-tag match-criteria.'
-if len(sys.argv[1:]) == 0:
-    # print help information and exit:
-    usage()
-    sys.exit(1)
-else:
-    try:
-        optlist, args = getopt.getopt(
-              sys.argv[1:],
-              "d:hs:t:v:",
-              [
-                  "days-old=",
-                  "help",
-                  "snapshot-size=",
-                  "tag-name=",
-                  "tag-value="
-              ]
-           )
-    except getopt.GetoptError as err:
-        # print help information and exit:
-        usage()
-        sys.exit(2)
+# If we pass a null value, assume safe to search for *any* value
+if tag_val == '':
+    tag_val == '*'
 
-    # Iterate the argument-list
-    for opt, arg in optlist:
-       if opt in ("-h", "--help"):
-            usage()
-            sys.exit()
-       elif opt in ( '-d', '--days-old='):
-           expiry_days = arg
-       elif opt in ( '-s', '--snapshot-size='):
-           snap_size = arg
-       elif opt in ( '-t', '--tag-name='):
-           tag_name = arg
-       elif opt in ( '-v', '--tag-value='):
-           tag_value = arg
-       else:
-           assert False, "unhandled option"
+# Do some time-deltas
+today = datetime.date.today()
+datefilter = today - datetime.timedelta(days=exp_days)
+print('Searching for snapshots older than deletion threshold-date ['
+    +
+    datefilter.strftime("%Y-%m-%d")
+    +
+    ']... '
+)
 
-    # Fail on missing mandatories
+# Narrow the list of candidate-snapshots by tag-name
+snapshots = ec2client.describe_snapshots(
+        Filters=[
+            {
+                'Name': 'tag:' + tag_key,
+                'Values': [
+                    '*' + tag_val + '*'
+                ]
+            }
+        ]
+    )
 
-response = ec2client.describe_snapshots()
+# Boto3 doesn't let you '<= DATE' filter: gotta iterate
+for snapshot in snapshots['Snapshots']:
+    snap_created = snapshot['StartTime']
+    snap_id = snapshot['SnapshotId']
 
+    if snap_created.strftime('%Y%m%d') <= datefilter.strftime("%Y%m%d"):
+        print(
+            'Snapshot %s created %s is %s days old or older: DELETING... ' %
+            (snap_id, snap_created.strftime('%Y-%m-%d') , exp_days),
+            end = ''
+        )
+
+        # Try to nuke the snap...
+        delstruct = ec2client.delete_snapshot(SnapshotId=snap_id)
+
+        # Print the request-status
+        if delstruct['ResponseMetadata']['HTTPStatusCode'] == 200:
+            print('Delete succeded')
+        else:
+            print('Delete failed')
+        sys.exit()
+
+    else:
+        print(
+            'Snapshot %s created %s is not %s days old or older: keeping' %
+            (snap_id, snap_created.strftime('%Y-%m-%d'), exp_days)
+        )
