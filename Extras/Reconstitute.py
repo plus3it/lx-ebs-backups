3f1f9a140a787d663353026401cd6b9af9c140c3
Thomas H Jones II <github@xanthia.com>
2019-09-17 16:39:12 -0400

    Add logic to start and status the recovery base-instance

--- Extras/Reconstitute.py
+++ Extras/Reconstitute.py
@@ -2,8 +2,59 @@
 import boto3
 import getopt
 import sys
+import time
 from optparse import OptionParser
 
+# Create initial recovery instance
+def mkRecovInst( amiId, ec2Type, provKey, ec2Snet, ec2Az, ec2Label):
+    launchResponseJson = ec2client.run_instances(
+        ImageId=amiId,
+        InstanceType=ec2Type,
+        KeyName=provKey,
+        MaxCount=1,
+        MinCount=1,
+        NetworkInterfaces=[
+            {
+                'DeviceIndex': 0,
+                'SubnetId': ec2Snet
+            }
+        ],
+        Placement={
+            'AvailabilityZone': ec2Az
+        },
+        TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {
+                        'Key': 'Name',
+                        'Value': ec2Label
+                    }
+                ]
+            }
+        ]
+    )
+
+    return launchResponseJson
+
+# Check recovery-instance's state
+def chkInstState(instanceId):
+    instanceInfo = ec2client.describe_instance_status(
+        InstanceIds=[
+            instanceId,
+        ],
+    )
+
+    if ( instanceInfo.get('InstanceStatuses') ):
+##         currentState = instanceInfo.get('InstanceStatuses')[0].get('InstanceState').get('Name')
+        currentState = instanceInfo.get('InstanceStatuses')[0].get('InstanceStatus').get('Status')
+    else:
+        currentState = 'NOT_READY'
+
+
+    print(currentState)
+    return currentState
+    
 
 # Make our connections to the service
 ec2client = boto3.client('ec2')
@@ -76,36 +127,18 @@ amiId = options.recovery_ami_id
 ebsType  = options.ebs_volume_type
 ec2Az = options.availability_zone
 ec2Label  = options.recovery_hostname
-ec2Subnet = options.deployment_subnet
+ec2Snet = options.deployment_subnet
 ec2Type = options.recovery_instance_type
-rescueKey  = options.provisioning_key
+provKey  = options.provisioning_key
 snapSearchVal = options.search_string
 
+recoveryHost = mkRecovInst(amiId, ec2Type, provKey, ec2Snet, ec2Az, ec2Label)
+recoveryHostInstanceStruct = recoveryHost.get('Instances', None)
+recoveryHostState = recoveryHostInstanceStruct[0].get('State', None).get('Code', None)
+recoveryHostInstanceId = recoveryHostInstanceStruct[0].get('InstanceId', None)
 
-launchResponseJson = ec2client.run_instances(
-    ImageId=amiId,
-    InstanceType=ec2Type,
-    KeyName=rescueKey,
-    MaxCount=1,
-    MinCount=1,
-    NetworkInterfaces=[
-        {
-            'DeviceIndex': 0,
-            'SubnetId': ec2Subnet
-        }
-    ],
-    Placement={
-        'AvailabilityZone': ec2Az
-    },
-    TagSpecifications=[
-        {
-            'ResourceType': 'instance',
-            'Tags': [
-                {
-                    'Key': 'Name',
-                    'Value': ec2Label
-                }
-            ]
-        }
-    ]
-)
+print('Instance ID: ' + recoveryHostInstanceId )
+
+while ( chkInstState(recoveryHostInstanceId) != 'ok' ):
+    print('Waiting for ' + recoveryHostInstanceId + 'to come online... ', end = '')
+    time.sleep(10)
